f21d06573aca328b40edfd0738f7867f411e6477
Yutaro Murata <oyu.cube@gmail.com>
2017-09-13 15:23:55 +0900

    train modify

--- model_at2.py
+++ model_at2.py
@@ -111,16 +111,16 @@ class SAF(chainer.Chain):
                 b = b1
 
         elif mode == 0:
-            l, s, b1 = self.first_forward(x, num_lm)
+            l, s, b1 = self.first_forward(x, num_lm, test=True)
             for i in range(n_step):
                 if i + 1 == n_step:
                     xm, lm, sm = self.make_img(x, l, s, num_lm, random=0)
-                    l1, s1, y, b = self.recurrent_forward(xm, lm, sm)
+                    l1, s1, y, b = self.recurrent_forward(xm, lm, sm, test=True)
                     accuracy = y.data * target.data
                     return xp.sum(accuracy)
                 else:
                     xm, lm, sm = self.make_img(x, l, s, num_lm, random=0)
-                    l1, s1, y, b = self.recurrent_forward(xm, lm, sm)
+                    l1, s1, y, b = self.recurrent_forward(xm, lm, sm, test=True)
                 l = l1
                 s = s1
 
@@ -149,8 +149,8 @@ class SAF(chainer.Chain):
             l_list[i] = l.data
         return
 
-    def first_forward(self, x, num_lm):
-        self.rnn_1(Variable(xp.zeros((num_lm, self.n_unit)).astype(xp.float32)))
+    def first_forward(self, x, num_lm, test=False):
+        self.rnn_1(Variable(xp.zeros((num_lm, self.n_unit)).astype(xp.float32), volatile=test))
         h2 = F.relu(self.l_norm_cc1(self.context_cnn_1(F.average_pooling_2d(x, 4, stride=4))))
         h3 = F.relu(self.l_norm_cc2(self.context_cnn_2(F.max_pooling_2d(h2, 2, stride=2))))
         h4 = F.relu(self.l_norm_cc3(self.context_cnn_3(F.max_pooling_2d(h3, 2, stride=2))))
@@ -159,13 +159,13 @@ class SAF(chainer.Chain):
 
         l = F.sigmoid(self.attention_loc(h5))
         s = F.sigmoid(self.attention_scale(h5))
-        b = F.sigmoid(self.baseline(Variable(h5.data)))
+        b = F.sigmoid(self.baseline(Variable(h5.data, volatile=test)))
         return l, s, b
 
-    def recurrent_forward(self, xm, lm, sm):
+    def recurrent_forward(self, xm, lm, sm, test=False):
         ls = xp.concatenate([lm.data, sm.data], axis=1)
-        hgl = F.relu(self.glimpse_loc(Variable(ls)))
-        hg1 = F.relu(self.l_norm_c1(self.glimpse_cnn_1(Variable(xm))))
+        hgl = F.relu(self.glimpse_loc(Variable(ls, volatile=test)))
+        hg1 = F.relu(self.l_norm_c1(self.glimpse_cnn_1(Variable(xm, volatile=test))))
         hg2 = F.relu(self.l_norm_c2(self.glimpse_cnn_2(hg1)))
         hg3 = F.relu(self.l_norm_c3(self.glimpse_cnn_3(F.max_pooling_2d(hg2, 2, stride=2))))
         hgf = F.relu(self.glimpse_full(hg3))
@@ -176,7 +176,7 @@ class SAF(chainer.Chain):
         l = F.sigmoid(self.attention_loc(hr2))
         s = F.sigmoid(self.attention_scale(hr2))
         y = F.softmax(self.class_full(hr1))
-        b = F.sigmoid(self.baseline(Variable(hr2.data)))
+        b = F.sigmoid(self.baseline(Variable(hr2.data, volatile=test)))
         return l, s, y, b
 
     # loss 関数を計算
